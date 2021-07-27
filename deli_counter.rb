f147693cc9f6792e607ddf3a5f7f125fd2ac44da
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-27 11:22:34 -0700

    Deli Counter - Take a Number assignment completed.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        customer_list = katz_deli.collect.with_index(1) do |customer, index| 
+            "#{index}. #{customer}"
+        end.join(" ")
+        puts "The line is currently: #{customer_list}"
+    end
+end
+
+def take_a_number(katz_deli, customer)
+    katz_deli.push(customer)
+    puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli.shift}." 
+    end
+end
